ff1cbb1833e95759cd7687d4315c67ed15d99d2c
mammad-J <109274744+mammad-J@users.noreply.github.com>
2022-12-24 23:06:49 +0330

    Fixed a bug!!!
    It was really annoying :)

--- client.py
+++ client.py
@@ -1,39 +1,29 @@
 import socket
-
+import threading
 
 HOST = "127.0.0.1"
 PORT = 6969
 encoding = "utf-8"
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-
-
-def recieving_messages(socket_instance):
+def get_message(client_socket):
     while True:
-        msg = socket_instance.recv(1024)
-        if msg:
-            print(msg.decode())
+        msg = str(input(">"))
+        if msg == "quit":
+            client_socket.close()
         else:
-            connection.close()
-            break
+            client_socket.send(bytes(msg, encoding))
 
 
 def client():
-    threading.Thread(target=recieving_messages, args=[s]).start()
-    print('Connected to chat!')
-
+    socket_instance = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_instance.connect((HOST, PORT))
+    print("Connected to server!")
+    threading.Thread(target=get_message, args=[socket_instance]).start()
     while True:
-        msg = input()
-        
-        if msg == 'quit':
-                break
-
-        socket_instance.send(bytes(msg, encoding))
-
-    socket_instance.close()
+        msg = socket_instance.recv(1024)
+        print(msg.decode(encoding))
 
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     client()
